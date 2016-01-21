1cd3e377a32d8433df99c9964871a0e652b8b0bf
Mauro Moreno <moreno.mauro.emanuel@gmail.com>
2016-01-21 23:44:00 +0000

    PHPUnit config

---
+++ GrepBundle.php
@@ -0,0 +1,17 @@
+<?php
+/**
+ * Class GrepBundle
+ *
+ * @author Mauro Moreno <moreno.mauro.emanuel@gmail.com>
+ */
+namespace MauroMoreno\GrepBundle;
+
+use Symfony\Component\HttpKernel\Bundle\Bundle;
+
+/**
+ * Class GrepBundle
+ * @package MauroMoreno\GrepBundle
+ */
+class GrepBundle extends Bundle
+{
+}
